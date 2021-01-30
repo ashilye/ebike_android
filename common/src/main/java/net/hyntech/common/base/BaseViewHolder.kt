5199c081649c9b5a04e95c391a5ffefbde0d91e4
gaoguanqi <307590625@qq.com>
2021-01-30 15:30:23 +0800

    ids

--- common/src/main/java/net/hyntech/common/base/BaseViewHolder.kt
+++ common/src/main/java/net/hyntech/common/base/BaseViewHolder.kt
@@ -1,13 +1,12 @@
 package net.hyntech.common.base
 
 import android.view.View
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import net.hyntech.common.R
 
 abstract class BaseViewHolder<T:Any>(itemView: View): RecyclerView.ViewHolder(itemView){
 
-    val itemRoot: View? = itemView.findViewById(R.id.common_item)
+    val itemRoot: View? = itemView.findViewById(R.id.common_item_root)
 
     abstract fun setData(pos:Int,data:T?)
 
