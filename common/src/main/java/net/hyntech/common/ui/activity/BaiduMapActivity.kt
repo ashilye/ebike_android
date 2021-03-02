ec5e1e26a067dfe9005fb6bb74f3b08357606a38
gaoguanqi <307590625@qq.com>
2021-03-02 17:32:41 +0800

    poi

--- common/src/main/java/net/hyntech/common/ui/activity/BaiduMapActivity.kt
+++ common/src/main/java/net/hyntech/common/ui/activity/BaiduMapActivity.kt
@@ -16,9 +16,17 @@ import com.baidu.location.LocationClient
 import com.baidu.location.LocationClientOption
 import com.baidu.mapapi.map.*
 import com.baidu.mapapi.model.LatLng
+import com.baidu.mapapi.search.core.PoiInfo
+import com.baidu.mapapi.search.core.SearchResult
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption
+import com.baidu.mapapi.search.poi.*
+import net.hyntech.baselib.utils.LogUtils
 import net.hyntech.common.R
 import net.hyntech.common.base.BaseActivity
+import net.hyntech.common.base.BaseAdapter
+import net.hyntech.common.ext.toVisible
 import net.hyntech.common.provider.ARouterConstants
+import net.hyntech.common.ui.adapter.PoiResultAdapter
 import net.hyntech.common.widget.map.baidu.MapViewHandler
 import net.hyntech.common.widget.map.baidu.MyLocationListener
 
@@ -29,11 +37,18 @@ import net.hyntech.common.widget.map.baidu.MyLocationListener
  *   -> poi 检索
  */
 @Route(path = ARouterConstants.BAIDU_MAP_PAGE)
-class BaiduMapActivity: BaseActivity(), SensorEventListener {
+class BaiduMapActivity: BaseActivity(), SensorEventListener, OnGetPoiSearchResultListener {
+
+    private var keyword:String? = ""
 
     private var mapView: TextureMapView? = null
     private var rvPoint: RecyclerView? = null
     private var baiduMap: BaiduMap? = null
+    private val poiAdapter: PoiResultAdapter by lazy { PoiResultAdapter(this).apply {
+        this.setListener(object: BaseAdapter.OnClickListener<PoiInfo>{
+            override fun onItemClick(pos: Int, item: PoiInfo?) {
+
+            } }) } }
 
     private val myMarker by lazy { BitmapDescriptorFactory.fromResource(R.drawable.ic_my_location) }
 
@@ -42,6 +57,9 @@ class BaiduMapActivity: BaseActivity(), SensorEventListener {
     //默认模式
     private val currentMode by lazy { MyLocationConfiguration.LocationMode.NORMAL }
     private var locData: MyLocationData? = null
+
+    //POI 检索
+    private val poiSearch by lazy { PoiSearch.newInstance().apply { setOnGetPoiSearchResultListener(this@BaiduMapActivity) } }
 
     private val locClient: LocationClient by lazy { LocationClient(this) }
     private val locListener: MyLocationListener by lazy {
@@ -83,6 +101,16 @@ class BaiduMapActivity: BaseActivity(), SensorEventListener {
             baiduMap?.setMapType(BaiduMap.MAP_TYPE_NORMAL)
             MapViewHandler(this@BaiduMapActivity).setMapView(it)
 
+            //点击地图上的位置检索
+            baiduMap?.setOnMapClickListener(object : BaiduMap.OnMapClickListener {
+                override fun onMapClick(latLng: LatLng?) {
+                    latLng?.let {
+                        updateMapState(latLng)
+                    }
+                }
+                override fun onMapPoiClick(poi: MapPoi?) {}
+            })
+
             //隐藏百度logo,ZoomControl
             val count: Int = it.childCount
             for (index in 1..count) {
@@ -110,21 +138,48 @@ class BaiduMapActivity: BaseActivity(), SensorEventListener {
     private fun receiveLocation(bdLocation: BDLocation) {
         bdLocation.let {
             radius = it.radius
+            this.keyword = bdLocation.addrStr
+
             lat = bdLocation.latitude
             lng = bdLocation.longitude
+            val ll = LatLng(lat,lng)
             val locData = MyLocationData.Builder().accuracy(radius) // 此处设置开发者获取到的方向信息，顺时针0-360
                     .direction(it.direction).latitude(it.latitude)
                     .longitude(it.longitude).build()
             baiduMap?.setMyLocationData(locData)
 
             val builder = MapStatus.Builder()
-            builder.target(LatLng(lat,lng)).zoom(18.0f)
+            builder.target(ll).zoom(18.0f)
             //跟随模式
             baiduMap?.setMyLocationConfiguration(
                 MyLocationConfiguration(currentMode, true, null)
             )
             baiduMap?.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()))
+
+            //开始检索
+            searchNeayBy(ll)
         }
+    }
+
+
+    private fun searchNeayBy(latLng: LatLng){
+        val option = PoiNearbySearchOption()
+        option.sortType = PoiSortType.distance_from_near_to_far
+        option.location(latLng)
+        option.mKeyword = this.keyword
+        if (radius <= 0.0f) {
+            option.mRadius = radius.toInt()
+        } else {
+            option.mRadius = 1000
+        }
+        poiSearch.searchNearby(option)
+    }
+
+    private fun updateMapState(latLng: LatLng) {
+        val options = MarkerOptions().position(latLng).icon(myMarker)
+        baiduMap?.clear()
+        baiduMap?.addOverlay(options)
+        searchNeayBy(latLng)
     }
 
     override fun onResume() {
@@ -164,4 +219,43 @@ class BaiduMapActivity: BaseActivity(), SensorEventListener {
             lastX = x.toDouble()
         }
     }
+
+
+    //------poi 检索--------
+    override fun onGetPoiIndoorResult(poiIndoorResult: PoiIndoorResult?) {}
+
+    //接受周边地理位置结果
+    override fun onGetPoiResult(result: PoiResult?) {
+        LogUtils.logGGQ("onGetPoiResult--error>>${result?.error}")
+        if (result == null || result.error == SearchResult.ERRORNO.RESULT_NOT_FOUND) {
+            //ToastUtil.showToast("未找到结果")
+            return
+        }
+
+        try {
+            if (result.error == SearchResult.ERRORNO.NO_ERROR) {
+                val list = result.allPoi
+                if (!list.isNullOrEmpty()) {
+                    rvPoint?.toVisible()
+                    poiAdapter.setData(list)
+                }
+            }else if(result.error == SearchResult.ERRORNO.AMBIGUOUS_KEYWORD){
+                //当输入关键字在本市没有找到，但在其他城市找到时，返回包含该关键字信息的城市列表
+                val s:StringBuilder = StringBuilder()
+                s.append("在")
+                result.suggestCityList?.forEach {
+                    s.append("${it.city},")
+                }
+                s.append("找到结果")
+                showToast(s.toString())
+            }
+        } catch (e: Exception) {
+            e.fillInStackTrace()
+            LogUtils.logGGQ("poi 检索 error ->${e.fillInStackTrace()}")
+        }
+    }
+
+    override fun onGetPoiDetailResult(poiDetailResult: PoiDetailResult?) {}
+
+    override fun onGetPoiDetailResult(poiDetailSearchResult: PoiDetailSearchResult?) {}
 }
---
+++ common/src/main/java/net/hyntech/common/ui/adapter/PoiResultAdapter.kt
@@ -0,0 +1,48 @@
+package net.hyntech.common.ui.adapter
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.baidu.mapapi.search.core.PoiInfo
+import net.hyntech.baselib.utils.UIUtils
+import net.hyntech.common.R
+import net.hyntech.common.base.BaseAdapter
+import net.hyntech.common.base.BaseViewHolder
+import net.hyntech.common.ext.layoutInflater
+
+class PoiResultAdapter(context: Context): BaseAdapter<PoiInfo,PoiResultAdapter.ViewHolder>(context) {
+
+    private val drawDefault:Drawable = UIUtils.getDrawable(R.drawable.ic_poi_default)
+    private val drawSelected:Drawable = UIUtils.getDrawable(R.drawable.ic_poi_selected)
+
+    var index:Int = 0
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = context.layoutInflater.inflate(R.layout.item_map_poi, parent, false)
+        val holder: PoiResultAdapter.ViewHolder = ViewHolder(view)
+        return holder
+    }
+
+    inner class ViewHolder(itemView: View): BaseViewHolder<PoiInfo>(itemView){
+        private var tvTitle: TextView? = itemView.findViewById(R.id.tv_title)
+        private var tvContent: TextView? = itemView.findViewById(R.id.tv_content)
+        private var ivPointState: ImageView? = itemView.findViewById(R.id.iv_point_state)
+
+        override fun setData(pos: Int, data: PoiInfo?) {
+            data?.let {
+                tvTitle?.text = it.name
+                tvContent?.text = it.address
+                if(pos == index){
+                    ivPointState?.background =  drawDefault
+                }else{
+                    ivPointState?.background =  drawSelected
+                }
+            }
+        }
+    }
+}
