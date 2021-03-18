0fc8055b8a68868c0b6c7f0e1bf226b253eaad7d
gaoguanqi <307590625@qq.com>
2021-03-18 08:37:02 +0800

    used

---
+++ common/src/main/java/net/hyntech/common/widget/dialog/UsedDialog.kt
@@ -0,0 +1,38 @@
+package net.hyntech.common.widget.dialog
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import net.hyntech.baselib.widget.dialog.BaseCenterDialog
+import net.hyntech.common.R
+
+/**
+ *
+ */
+class UsedDialog(context: Context,
+                 isCancelable:Boolean = false, val listener: OnClickListener): BaseCenterDialog(context,isCancelable = isCancelable,style = R.style.CommonDialogStyle) {
+
+    override fun getLayoutId(): Int = R.layout.dialog_used
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+        findViewById<Button>(R.id.btn_cancle).let {
+            it.setOnClickListener {
+                this.onCancel()
+                listener.onCancelClick()
+            }
+        }
+        findViewById<Button>(R.id.btn_confirm).let {
+            it.setOnClickListener {
+                this.onCancel()
+                listener.onConfirmClick()
+            }
+        }
+
+    }
+
+    interface OnClickListener{
+        fun onCancelClick(){}
+        fun onConfirmClick()
+    }
+}
--- test/src/main/java/net/hyntech/test/ui/activity/SplashActivity.kt
+++ test/src/main/java/net/hyntech/test/ui/activity/SplashActivity.kt
@@ -10,19 +10,39 @@ import net.hyntech.baselib.utils.RequestPermission
 import net.hyntech.common.app.global.Constants
 import net.hyntech.common.base.BaseActivity
 import net.hyntech.common.widget.dialog.CommonDialog
+import net.hyntech.common.widget.dialog.UsedDialog
 import net.hyntech.test.R
 
 class SplashActivity : BaseActivity() {
 
+    private val hasWelcome by lazy {
+        SPUtils.getInstance(BaseApp.instance.getAppPackage()).getBoolean(Constants.SaveInfoKey.HAS_WELCOME_TEST,false)
+    }
+
     private var dialog: CommonDialog? = null
+
+    private val usedDialog by lazy { UsedDialog(this,false,object : UsedDialog.OnClickListener {
+        override fun onConfirmClick() {
+            applyPermissions()
+        }
+    }) }
 
     private val rxPermissions: RxPermissions by lazy { RxPermissions(this) }
 
     override fun getLayoutId(): Int = R.layout.activity_splash
 
     override fun initData(savedInstanceState: Bundle?) {
-        applyPermissions()
+        if(hasWelcome){
+            applyPermissions()
+        }else{
+            showUsedDialog()
+        }
     }
+
+    private fun showUsedDialog() {
+        usedDialog.show()
+    }
+
 
     override fun onRestart() {
         super.onRestart()
@@ -46,7 +66,7 @@ class SplashActivity : BaseActivity() {
     }
 
     private fun launchTarget() {
-        if(SPUtils.getInstance(BaseApp.instance.getAppPackage()).getBoolean(Constants.SaveInfoKey.HAS_WELCOME_TEST,false)){
+        if(hasWelcome){
             onStartActivity(AccountActivity::class.java,isFinish = true)
         }else{
             onStartActivity(WelcomeActivity::class.java,isFinish = true)
